d64f3c4765ab407b5ee2673959d3c5fd1b8d53af
Karl Wang <karl.wang@solinnov.com.au>
2019-11-06 15:17:23 +1030

    Axios working

--- flask/main.py
+++ flask/main.py
@@ -14,8 +14,8 @@ cur_json = ''
 
 api = Api(app,
           version='1.0',
-          title='Octopus Target Generator',
-          description='Octopus Target Generator RESTful API',
+          title='Mental Maths Practice',
+          description='Mental Maths Practice (MMP) RESTful API',
           doc='/apis')
 
 
@@ -25,56 +25,6 @@ class QuestionFactory(Resource):
     def get(self):
         res = {"questions": ["1 + 1", "2 + 2", "3 + 3"]}
         return res
-
-
-@api.route('/json_data')
-class JsonData(Resource):
-    def get(self):
-        '''Get the content of the current selected JSON file'''
-        json_data = {}
-        if cur_json != '':
-            with open(json_folder + '/' + cur_json) as json_file:
-                json_data = json.load(json_file)
-        return json_data
-
-    def post(self):
-        '''Save to current JSON file'''
-        data_posted = request.json['body']
-        with open(json_folder + '/' + cur_json, 'w') as outfile:
-            json.dump(data_posted, outfile)
-        print("Data posted: {}".format(data_posted))
-        return request.json, 201
-
-
-@api.route('/file_list')
-class ListFiles(Resource):
-    def get(self):
-        files = []
-        ret_json = {"folder": "", "files": []}
-        # r=root, d=directories, f = files
-        for r, d, f in os.walk(json_folder):
-            for file in f:
-                if '.json' in file:
-                    files.append(file)
-
-        ret_json["files"] = files
-        ret_json["folder"] = os.path.abspath(json_folder)
-        print(ret_json)
-        return ret_json
-
-
-@api.route('/json_file')
-class JsonFile(Resource):
-    def get(self):
-        '''Get the current JSON file name'''
-        return cur_json
-
-    def post(self):
-        '''Set the current JSON file'''
-        global cur_json
-        cur_json = request.json['body']['json_file']
-        print('Set current JSON file to {}'.format(cur_json))
-        return cur_json, 201
 
 
 if __name__ == '__main__':
--- vue/mmp_frontend/src/components/Question.vue
+++ vue/mmp_frontend/src/components/Question.vue
@@ -5,7 +5,8 @@
             <p class="display-1 text--primary">test card title</p>
             <p>adjective</p>
             <div class="text--primary">
-                {{curQuestion}} <br />
+                {{curQuestion}}
+                <br />
                 <input type="text" @keyup.enter="onSubmit" />
             </div>
         </v-card-text>
@@ -16,30 +17,41 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     data: () => ({
         questionID: Number,
-        questions: Array,
+        questions: Array
     }),
-    props: {
-
-    },
+    props: {},
     computed: {
-        curQuestion: function () {
+        curQuestion: function() {
             return this.questions[this.questionID];
         }
     },
     methods: {
-        onNext: function () {
+        onNext: function() {
             this.questionID++;
         },
-        onSubmit: function () {
-            alert('submit');
+        onSubmit: function() {
+            alert("submit");
         }
     },
-    mounted () {
+    mounted() {
         this.questionID = 0;
         this.questions = ["one", "two", "three"];
+        axios
+            .get("http://" + location.hostname + ":5000/questions")
+            .then(response => {
+                alert(response.data.questions);
+                // this.jsonFiles = response.data.files;
+                // this.folder = response.data.folder;
+                // this.$store.dispatch("updateFolderAction", this.folder);
+            })
+            .catch(e => {
+                alert(e);
+            });
     }
 };
 </script>
